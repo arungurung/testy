1b940ede5ca47f7c78975561b21d28f5c741eb1b
arun <arun.gurung@bigtincan.com>
2023-07-28 02:23:58 +1000

    Updates

---
+++ _app/immutable/chunks/paths.184da198.js
@@ -0,0 +1,1 @@
+var s;const e=((s=globalThis.__sveltekit_503id6)==null?void 0:s.base)??"/testy";var t;const a=((t=globalThis.__sveltekit_503id6)==null?void 0:t.assets)??e;export{a,e as b};
--- _app/immutable/chunks/paths.b6aa03f8.js
+++
@@ -1,1 +0,0 @@
-var s;const t=((s=globalThis.__sveltekit_dsqe1l)==null?void 0:s.base)??"/testy";var e;const a=((e=globalThis.__sveltekit_dsqe1l)==null?void 0:e.assets)??t;export{a,t as b};
